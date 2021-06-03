5dcc0ee5e02966b6d2e9eddb69c2a94b080b6a15
Dave Baskin <dfbaskin@gmail.com>
2021-06-03 10:54:42 -0400

    Updating to DAPR v1.2

--- packages/dapr-initialization/initialize-dapr-redis.ps1
+++ packages/dapr-initialization/initialize-dapr-redis.ps1
@@ -1,4 +1,8 @@
 kubectl create ns dapr-redis
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
 helm install redis bitnami/redis -n dapr-redis
+
 kubectl apply -f @(Join-Path $PSScriptRoot "redis-state.yaml")
 kubectl apply -f @(Join-Path $PSScriptRoot "redis-pubsub.yaml")
